4f0973a3fb92938ac646d80c39a8aa94292ce152
Naliv88 <nalivpv@gmail.com>
2022-07-28 21:03:58 +0300

    hw 8.5

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,6 +2,7 @@
 	const menuBtnRef = document.querySelector("[data-menu-button]");
 	const mobileMenuRef = document.querySelector("[data-menu]");
 	const body = document.querySelector("[data-body]");
+	const logo = document.querySelector("[data-logo]");
 
 	menuBtnRef.addEventListener("click", () => {
 		const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
@@ -11,5 +12,6 @@
 
 		mobileMenuRef.classList.toggle("is-open");
 		body.classList.toggle("no-scroll");
+		logo.classList.toggle("no-show");
 	});
 })();
